bc9658d2a7cafbd15801600293b2663da97eb734
cneben <benoit@qanava.org>
2016-03-21 16:19:06 +0100

    Squashed 'GTpo/' content from commit ed9ad1f
    git-subtree-dir: GTpo
    git-subtree-split: ed9ad1f81f372a8c19577960496d4c3b8e670947

---
+++ samples/basic/basic.cpp
@@ -0,0 +1,64 @@
+/*
+    The MIT License (MIT)
+
+    Copyright (c) 2016 Benoit AUTHEMAN
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+//-----------------------------------------------------------------------------
+// This file is a part of the GTpo software library.
+//
+// \file	basic.cpp
+// \author	benoit@qanava.org
+// \date	2016 01 25
+//-----------------------------------------------------------------------------
+
+// STD headers
+#include <random>
+
+// GTpo headers
+#include "gtpoGmlSerializer.h"
+#include "gtpoRandomGraph.h"
+#include "basic.h"
+
+int	main( int /*argc*/, char** /*argv*/ )
+{
+    stpo::Graph sg;
+    auto n1 = sg.createNode();
+
+    RandomGraph::RandomConfig rc;
+    rc.nodeCount = 10;
+    rc.outNodeRng = std::make_pair( 1, 3 );
+    RandomGraph::generate( sg, rc );
+
+    stpo::Graph::SharedNode ownedN1 = n1.lock();
+    if ( ownedN1 ) {
+        double nx = stpo::SConfig::getNodeX( ownedN1 );
+        double ny = stpo::SConfig::getNodeY( ownedN1 );
+        double nw = stpo::SConfig::getNodeWidth( ownedN1 );
+        double nh = stpo::SConfig::getNodeHeight( ownedN1 );
+        std::cout << "nx=" << nx << " ny=" << ny << " nw=" << nw << " nh=" << nh << std::endl;
+    }
+    auto n2 = sg.createNode();
+    std::cout << sg.getRootNodeCount() << std::endl;
+
+    return 0;
+}
+
---
+++ src/gtpoRandomGraph.h
@@ -0,0 +1,101 @@
+/*
+    This file is part of Quick Qanava library.
+
+    Copyright (C) 2008-2015 Benoit AUTHEMAN
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//-----------------------------------------------------------------------------
+// This file is a part of the GTpo software library.
+//
+// \file	gtpoRandomGraph.h
+// \author	benoit@qanava.org
+// \date	2016 02 10
+//-----------------------------------------------------------------------------
+
+#ifndef gtpoRandomGraph_h
+#define gtpoRandomGraph_h
+
+// STD headers
+#include <random>
+#include <cstdlib>
+
+// GTPO headers
+#include "./gtpoUtils.h"
+
+class RandomGraph
+{
+public:
+    struct RandomConfig {
+        std::string nodeClassName   = "gtpo::Node";
+        std::string edgeClassName   = "gtpo::Edge";
+        int nodeCount               = 10;
+        std::pair<int,int>          outNodeRng{ 1, 3 };
+
+        std::pair<double, double>   xRng{ 0., 500. };
+        std::pair<double, double>   yRng{ 0., 500. };
+        std::pair<double, double>   widthRng{ 70., 120. };
+        std::pair<double, double>   heightRng{ 40., 50. };
+
+        auto    isValid() const -> bool {
+            return ( ( std::abs( widthRng.first - widthRng.second ) ) > 0.000001 &&
+                     ( std::abs( heightRng.first -  heightRng.second ) > 0.000001 ) );  // We do not need precise epsilon comparison here
+        }
+    };
+
+    template < class Graph >
+    static void generate( Graph& graph, RandomConfig& config )
+    {
+        if ( !config.isValid() )
+            return;
+
+        std::vector< typename Graph::WeakNode > nodes;
+        for ( int n = 0; n < config.nodeCount; ++n ) {
+            auto node = graph.createNode( config.nodeClassName );
+            if ( !node.expired() )
+                nodes.push_back( node );
+        }
+        if ( nodes.size() < 2 )
+            return;
+        std::random_device rd;
+        std::mt19937 gen( 200 );
+        std::uniform_int_distribution< int > outNodeDist( config.outNodeRng.first, config.outNodeRng.second );
+        std::uniform_int_distribution< int > nodeIndexDist( 0, (int)nodes.size()-1 );
+        for ( auto source: nodes ) {
+            int outNodeCount = outNodeDist( gen );
+            for ( int outNode = 0; outNode < outNodeCount; ++outNode ) {
+                auto destination = nodes[ nodeIndexDist( gen ) ];
+                if ( !gtpo::compare_weak_ptr( source, destination ) )
+                graph.createEdge( config.edgeClassName, source, destination );
+            }
+        }
+
+        std::uniform_real_distribution< double > xDist( config.xRng.first, config.xRng.second );
+        std::uniform_real_distribution< double > yDist( config.yRng.first, config.yRng.second );
+        std::uniform_real_distribution< double > widthDist( config.widthRng.first, config.widthRng.second );
+        std::uniform_real_distribution< double > heightDist( config.heightRng.first, config.heightRng.second );
+        for ( auto weakNode: nodes ) {
+            typename Graph::SharedNode node = weakNode.lock();
+            if ( node ) {
+                Graph::Configuration::setNodeX( node, xDist( gen ) );
+                Graph::Configuration::setNodeY( node, yDist( gen ) );
+                Graph::Configuration::setNodeWidth( node, widthDist( gen ) );
+                Graph::Configuration::setNodeHeight( node, heightDist( gen ) );
+            }
+        }
+    }
+};
+
+#endif // gtpoRandomGraph_h
