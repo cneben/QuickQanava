54225e3c94f2afd2c93baf62d8d20cc76071e9fa
cneben <benoit@destrat.io>
2022-02-15 13:13:22 +0100

    #148 Reactivate behaviours... Simplify existing observable/observer code.
    Signed-off-by: cneben <benoit@destrat.io>

--- src/gtpo/behaviourable.h
+++ src/gtpo/behaviourable.h
@@ -64,7 +64,7 @@ public:
  *
  * \nosubgrouping
  */
-template <class behaviour_t>
+template <class observer_t>
 class observable : public abstract_observable
 {
     /*! \name behaviourable Object Management *///-----------------------------
@@ -72,8 +72,8 @@ class observable : public abstract_observable
 public:
     observable() : abstract_observable() { }
     ~observable() noexcept { _observers.clear(); }
-    observable(const observable<behaviour_t>&) = default;
-    observable& operator=(const observable<behaviour_t>&) = default;
+    observable(const observable<observer_t>&) = default;
+    observable& operator=(const observable<observer_t>&) = default;
 
 public:
     //! Clear all registered behaviours (they are automatically deleted).
@@ -93,19 +93,19 @@ public:
      *   sg.addBehaviour( std::make_unique< MyBehaviour >( ) );
      * \endcode
      */
-    inline auto     add_observer(std::unique_ptr<behaviour_t> observer) -> void {
+    inline auto     add_observer(std::unique_ptr<observer_t> observer) -> void {
         _observers.emplace_back(std::move(observer));
     }
 
     //! std::vector of std::unique_ptr pointers on behaviour.
-    using behaviours_t = std::vector<std::unique_ptr<behaviour_t>>;
+    using observers_t = std::vector<std::unique_ptr<observer_t>>;
 
 public:
     //! Return true if an observer is currently registered (ie getObservers().size() > 0).
     inline auto    hasObservers() const noexcept -> bool { return _observers.size() > 0; }
 
     //! Return a read only container of actually registered observers.
-    inline auto    getObservers() const noexcept -> const behaviours_t& { return _observers; }
+    inline auto    getObservers() const noexcept -> const observer_t& { return _observers; }
 
 protected:
     /*! \brief Call a pointer on a method on all registered dynamic behaviours.
@@ -117,94 +117,48 @@ protected:
      * \endcode
      */
     template < class T >
-    auto    notify_observers( void (behaviour_t::*method)(T&) /*noexcept*/, T& arg ) noexcept -> void;
+    auto    notify_observers( void (observer_t::*method)(T&) /*noexcept*/, T& arg ) noexcept -> void;
 
     template < class T, class T2 >
-    auto    notify_observers( void (behaviour_t::*method)(T&, T2&) /*noexcept*/, T& arg, T2&) noexcept -> void;
+    auto    notify_observers( void (observer_t::*method)(T&, T2&) /*noexcept*/, T& arg, T2&) noexcept -> void;
 
     template < class T, class T2, class T3 >
-    auto    notify_observers( void (behaviour_t::*method)(T&, T2&, const T3&) /*noexcept*/, T&, T2&, const T3&) noexcept -> void;
+    auto    notify_observers( void (observer_t::*method)(T&, T2&, const T3&) /*noexcept*/, T&, T2&, const T3&) noexcept -> void;
 
     //! Similar to notifyBahaviours() but without arguments.
-    auto    notify_observers0( void (behaviour_t::*method)() /*noexcept*/ ) noexcept -> void;
+    auto    notify_observers0( void (observer_t::*method)() /*noexcept*/ ) noexcept -> void;
 
 private:
-    behaviours_t    _observers;
+    observers_t    _observers;
     //@}
     //-------------------------------------------------------------------------
 };
 
-/*! \brief Abstract interface for dynamic (virtual) node behaviours.
- *
- */
-template <class node_t, edge_t>
-class node_observer : public node_observer<config_t>
-{
-public:
-    node_observer() noexcept : node_behaviour<config_t>{} { }
-    virtual ~node_observer() = default;
-    node_observer(const node_observer<config_t>& ) = delete;
-    node_observer& operator=(const node_observer<config_t>&) = delete;
-    node_observer(node_observer<config_t>&&) = delete;
-    node_observer& operator=(node_observer<config_t>&&) = delete;
 
-public:
-    void    in_node_inserted(node_t& target, node_t& inNode, const edge_t& edge )  noexcept { on_in_node_inserted(target, weakInNode, edge); }
-    void    in_node_removed(node_t& target, node_t& inNode, const edge_t& edge )  noexcept { on_in_node_removed(target, weakInNode, edge); }
-    void    in_node_removed(node_t& target)  noexcept { on_in_node_removed(target); }
-    void    out_node_inserted(node_t& target, node_t& weakOutNode, const edge_t& edge )  noexcept { on_out_node_inserted(target, weakOutNode, edge); }
-    void    out_node_removed(node_t& target, node_t& weakOutNode, const edge_t& edge )  noexcept { on_out_node_removed(target, weakOutNode, edge); }
-    void    out_node_removed(node_t& target)  noexcept { on_out_node_removed(target); }
-
-protected:
-    /*! \brief Called immediatly after an in-edge with source \c weakInNode has been inserted.
-     */
-    virtual void    on_in_node_inserted(weak_node_t& target, weak_node_t& weakInNode, const weak_edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakInNode); static_cast<void>(edge); }
-
-    /*! \brief Called when an in-edge with source \c weakInNode is about to be removed.
-     */
-    virtual void    on_in_node_removed(weak_node_t& target, weak_node_t& weakInNode, const weak_edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakInNode); static_cast<void>(edge); }
-
-    /*! \brief Called immediatly after an in node has been removed.
-     */
-    virtual void    on_in_node_removed(weak_node_t& target)  noexcept { static_cast<void>(target); }
-
-    /*! \brief Called immediatly after an out-edge with destination \c weakOutNode has been inserted.
-     */
-    virtual void    on_out_node_inserted(weak_node_t& target, weak_node_t& weakOutNode, const weak_edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakOutNode); static_cast<void>(edge); }
-
-    /*! \brief Called when an out-edge with destination \c weakOutNode is about to be removed.
-     */
-    virtual void    on_out_node_removed(weak_node_t& target, weak_node_t& weakOutNode, const weak_edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakOutNode); static_cast<void>(edge); }
-
-    /*! \brief Called immediatly after an out-edge has been removed.
-     */
-    virtual void    on_out_node_removed(weak_node_t& target)  noexcept { static_cast<void>(target); }
-};
-
+template <class node_t, class edge_t>
+class node_observer;
 
 /*! \brief Observation interface for gtpo::node.
  *
  * \nosubgrouping
  */
 template <class node_t, class edge_t>
-class behaviourable_node : public behaviourable<gtpo::node_observer<config_t>,
-                                                typename config_t::node_behaviours
-                                               > // gtpo::behaviourable<>
+class observable_node : public observable<gtpo::node_observer<node_t, edge_t> >
 {
     /*! \name behaviourable_node Object Management *///------------------------
     //@{
 public:
-    behaviourable_node() noexcept { }
-    ~behaviourable_node() noexcept = default;
-    behaviourable_node(const behaviourable_node<node_t, edge_t>&) = delete;
-    behaviourable_node& operator=(const behaviourable_node<node_t, edge_t>&) = delete;
+    observable_node() noexcept { }
+    ~observable_node() noexcept = default;
+    observable_node(const observable_node<node_t, edge_t>&) = delete;
+    observable_node& operator=(const observable_node<node_t, edge_t>&) = delete;
 
     /*! \name Notification Helper Methods *///---------------------------------
     //@{
 public:
-    auto    add_node_observer(std::unique_ptr<node_observer_t> behaviour) -> void {
-        behaviourable<node_observer_t, node_static_behaviours_t>::add_behaviour(std::move(behaviour));
+    using super_t = observable<gtpo::node_observer<node_t, edge_t> >;
+    auto    add_node_observer(std::unique_ptr<gtpo::node_observer<node_t, edge_t>> observer) -> void {
+        super_t::add_behaviour(std::move(observer));
     }
 
     auto    notify_in_node_inserted(node_t target, node_t node, const edge_t& edge) noexcept -> void;
@@ -222,6 +176,108 @@ public:
     //-------------------------------------------------------------------------
 };
 
+/*! \brief Base class for concrete observers.
+ */
+template <class target_t>
+class observer
+{
+public:
+    observer() noexcept { }
+    ~observer() noexcept { }
+
+   observer(const observer& rhs) = default;
+   observer& operator=(const observer& rhs) = default;
+   observer(observer&& rhs) noexcept = default;
+   observer& operator=(observer&& rhs) noexcept = default;
+
+public:
+    target_t*   get_target() noexcept { return _target; }
+    void        set_target(target_t* target) { _target = target; }
+protected:
+    target_t*   _target = nullptr;
+
+public:
+    inline auto getName() const noexcept -> const std::string& { return _name; }
+protected:
+    inline auto setName(const std::string& name)  noexcept -> void { _name = name; }
+private:
+    std::string _name = "";
+
+public:
+    /*! \brief Enable this behaviour until it is disabled again with a call to disable().
+     *
+     * \note enabling a disabled behaviour does not apply behaviour on changes made
+     *       while it was disabled.
+     * \sa isEnabled() for enabled state description.
+     */
+    inline auto     enable() noexcept -> void { _enabled = true; }
+    /*! \brief Disable this behaviour until it is enabled back wit a call to enable().
+     *
+     * \sa isEnabled() for enabled state description.
+     */
+    inline auto     disable() noexcept -> void { _enabled = false; }
+    /*! \brief Return the actual "enabled" state for this behaviour.
+     *
+     *  This method could be usefull in very specific use cases, such as serialization or insertion of a large number
+     *  of nodes in a graph or group where this behaviour is applied.
+     *  A behaviour is enabled by default after its creation.
+     *
+     *  \return true if enable() has been called, false if disable() has been called.
+     */
+    inline auto     isEnabled() const noexcept -> bool { return _enabled; }
+
+protected:
+    bool            _enabled = true;
+};
+
+
+/*! \brief Abstract interface for dynamic (virtual) node behaviours.
+ *
+ */
+template <class node_t, class edge_t>
+class node_observer : public observer<node_t>
+{
+public:
+    node_observer() noexcept : observer<node_t>{} { }
+    virtual ~node_observer() = default;
+    node_observer(const node_observer<node_t, edge_t>& ) = delete;
+    node_observer& operator=(const node_observer<node_t, edge_t>&) = delete;
+    node_observer(node_observer<node_t, edge_t>&&) = delete;
+    node_observer& operator=(node_observer<node_t, edge_t>&&) = delete;
+
+public:
+    void    in_node_inserted(node_t& target, node_t& in_node, const edge_t& edge )  noexcept { on_in_node_inserted(target, in_node, edge); }
+    void    in_node_removed(node_t& target, node_t& in_node, const edge_t& edge )  noexcept { on_in_node_removed(target, in_node, edge); }
+    void    in_node_removed(node_t& target)  noexcept { on_in_node_removed(target); }
+    void    out_node_inserted(node_t& target, node_t& out_node, const edge_t& edge )  noexcept { on_out_node_inserted(target, out_node, edge); }
+    void    out_node_removed(node_t& target, node_t& out_node, const edge_t& edge )  noexcept { on_out_node_removed(target, out_node, edge); }
+    void    out_node_removed(node_t& target)  noexcept { on_out_node_removed(target); }
+
+protected:
+    /*! \brief Called immediatly after an in-edge with source \c weakInNode has been inserted.
+     */
+    virtual void    on_in_node_inserted(node_t& target, node_t& weakInNode, const edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakInNode); static_cast<void>(edge); }
+
+    /*! \brief Called when an in-edge with source \c weakInNode is about to be removed.
+     */
+    virtual void    on_in_node_removed(node_t& target, node_t& weakInNode, const edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakInNode); static_cast<void>(edge); }
+
+    /*! \brief Called immediatly after an in node has been removed.
+     */
+    virtual void    on_in_node_removed(node_t& target)  noexcept { static_cast<void>(target); }
+
+    /*! \brief Called immediatly after an out-edge with destination \c weakOutNode has been inserted.
+     */
+    virtual void    on_out_node_inserted(node_t& target, node_t& weakOutNode, const edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakOutNode); static_cast<void>(edge); }
+
+    /*! \brief Called when an out-edge with destination \c weakOutNode is about to be removed.
+     */
+    virtual void    on_out_node_removed(node_t& target, node_t& weakOutNode, const edge_t& edge)  noexcept { static_cast<void>(target); static_cast<void>(weakOutNode); static_cast<void>(edge); }
+
+    /*! \brief Called immediatly after an out-edge has been removed.
+     */
+    virtual void    on_out_node_removed(node_t& target)  noexcept { static_cast<void>(target); }
+};
 
 } // ::gtpo
 
