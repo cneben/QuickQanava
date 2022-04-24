e9b16718a85a983182fec9a67b7cb36c20602184
cneben <benoit@destrat.io>
2022-04-25 00:05:22 +0200

    #148 All tests reactivated.
    Signed-off-by: cneben <benoit@destrat.io>

--- tests/groups_tests.cpp
+++ tests/groups_tests.cpp
@@ -140,50 +140,52 @@ TEST(gtpo_group, ungroup_ungrouped_node)
     EXPECT_FALSE(g.ungroup_node(n, group));
 }
 
-/*
-TEST(gtpo_group, groupRootNodeContract)
+TEST(gtpo_group, group_root_node)
 {
     // Ensure that graph root node count is not modified when a node is grouped
     // CONTRACT: A node with zero in-degree is a root node even when it is grouped.
     qan::Graph g;
-    auto group = g.create_group().lock();
-    ASSERT_TRUE(group);
-    EXPECT_EQ(g.get_root_node_count(), 0);
-    auto n = g.create_node();
+    auto group = new qan::Group{};
+    g.insert_group(group);
     EXPECT_EQ(g.get_root_node_count(), 1);
+    auto n = g.create_node();
+    g.insert_node(n);
+    EXPECT_EQ(g.get_root_node_count(), 2);
 
     g.group_node(n, group);
-    EXPECT_EQ( group->get_node_count(), 1 );
-    EXPECT_EQ( g.get_group_count(), 1 );
-    EXPECT_EQ(g.get_root_node_count(), 1);
+    EXPECT_EQ(group->get_node_count(), 1);
+    EXPECT_EQ(g.get_group_count(), 1);
+    EXPECT_EQ(g.get_root_node_count(), 2);
 
     g.ungroup_node(n, group);
-    EXPECT_EQ( group->get_node_count(), 0 );
-    EXPECT_EQ(g.get_root_node_count(), 1);
+    EXPECT_EQ(group->get_node_count(), 0);
+    EXPECT_EQ(g.get_root_node_count(), 2);
 }
 
-TEST(gtpo_group, groupGroup)
+TEST(gtpo_group, group_group)
 {
     qan::Graph g;
-    auto group1 = g.create_group().lock();
-    auto group2 = g.create_group().lock();
-    using weak_group_t = gtpo::group<>::weak_group_t;
-    EXPECT_EQ( group1->get_node_count(), 0 );
-    g.group_node(weak_group_t{group2}, group1);
-    EXPECT_EQ( group1->get_node_count(), 1 );
+    auto group1 = new qan::Group{};
+    g.insert_group(group1);
+    auto group2 = new qan::Group{};
+    g.insert_group(group2);
+    EXPECT_EQ(group1->get_node_count(), 0);
+    g.group_node(group2, group1);
+    EXPECT_EQ(group1->get_node_count(), 1);
 }
 
-TEST(gtpo_group, ungroupGroup)
+TEST(gtpo_group, ungroup_group)
 {
     qan::Graph g;
-    auto group1 = g.create_group().lock();
-    auto group2 = g.create_group().lock();
-    using weak_group_t = gtpo::group<>::weak_group_t;
-    g.group_node(weak_group_t{group2}, group1);
-    EXPECT_EQ( group1->get_node_count(), 1 );
-    EXPECT_EQ( g.get_group_count(), 2 );
-    g.ungroup_node(weak_group_t{group2}, group1);
-    EXPECT_EQ( group1->get_node_count(), 0 );
+    auto group1 = new qan::Group{};
+    g.insert_group(group1);
+    auto group2 = new qan::Group{};
+    g.insert_group(group2);
+    g.group_node(group2, group1);
+    EXPECT_EQ(group1->get_node_count(), 1);
+    EXPECT_EQ(g.get_group_count(), 2);
+    g.ungroup_node(group2, group1);
+    EXPECT_EQ(group1->get_node_count(), 0);
 }
 
 TEST(gtpo_group, clearTopology)
@@ -192,43 +194,47 @@ TEST(gtpo_group, clearTopology)
     // topology
     qan::Graph g;
     auto n1 = g.create_node();
+    g.insert_node(n1);
     auto n2 = g.create_node();
-    auto e1 = g.create_edge(n1, n2);
-    auto g1 = g.create_group();
-    EXPECT_EQ( g.get_node_count(), 2 );
-    EXPECT_EQ( g.get_group_count(), 1 );
-    EXPECT_EQ( g.get_edge_count(), 1 );
-    g.group_node( n1, g1 );
-    g.group_node( n2, g1 );
+    g.insert_node(n2);
+    g.insert_edge(n1, n2);
+    auto g1 = new qan::Group{};
+    g.insert_group(g1);
+    EXPECT_EQ(g.get_node_count(), 3);
+    EXPECT_EQ(g.get_group_count(), 1);
+    EXPECT_EQ(g.get_edge_count(), 1);
+    g.group_node(n1, g1);
+    g.group_node(n2, g1);
     g.clear();
-    EXPECT_TRUE( n1.expired() );
-    EXPECT_EQ( g.get_node_count(), 0 );
-    EXPECT_EQ( g.get_group_count(), 0 );
-    EXPECT_EQ( g.get_edge_count(), 0 );
-    EXPECT_TRUE( g1.expired() );
+    EXPECT_EQ(g.get_node_count(), 0);
+    EXPECT_EQ(g.get_group_count(), 0);
+    EXPECT_EQ(g.get_edge_count(), 0);
 }
 
-TEST(gtpo_group, remove_groupTopology)
+TEST(gtpo_group, remove_group_topology)
 {
     // TEST: removing a group with content should preserve group topology
     qan::Graph g;
     auto n1 = g.create_node();
+    g.insert_node(n1);
     auto n2 = g.create_node();
-    auto e1 = g.create_edge(n1, n2);
-    EXPECT_EQ( g.get_node_count(), 2 );
-    EXPECT_EQ( g.get_edge_count(), 1 );
+    g.insert_node(n2);
+    g.insert_edge(n1, n2);
+    EXPECT_EQ(g.get_node_count(), 2);
+    EXPECT_EQ(g.get_edge_count(), 1);
 
-    auto g1 = g.create_group();
-    EXPECT_EQ( g.get_group_count(), 1 );
-    g.group_node( n1, g1 );
-    g.group_node( n2, g1 );
+    auto g1 = new qan::Group{};
+    g.insert_group(g1);
+    EXPECT_EQ(g.get_group_count(), 1);
+    g.group_node(n1, g1);
+    g.group_node(n2, g1);
 
     g.remove_group(g1);
-    EXPECT_EQ( g.get_group_count(), 0 );
+    EXPECT_EQ(g.get_group_count(), 0);
 
     // Group topology should remain in the graph
-    EXPECT_EQ( g.get_node_count(), 2 );
-    EXPECT_EQ( g.get_edge_count(), 1 );
+    EXPECT_EQ(g.get_node_count(), 2);
+    EXPECT_EQ(g.get_edge_count(), 1);
     g.clear();
 }
 
@@ -236,74 +242,88 @@ TEST(gtpo_group, remove_groupTopology)
 // GTpo Groups adjacent node/edge
 //-----------------------------------------------------------------------------
 
-TEST(gtpo_group, adjacentEdgesSimple)
+TEST(gtpo_group, adjacent_edges)
 {
     qan::Graph g;
     auto n1 = g.create_node();
+    g.insert_node(n1);
     auto n2 = g.create_node();
+    g.insert_node(n2);
 
-    auto group1 = g.create_group().lock();
+    auto group1 = new qan::Group{};
+    g.insert_group(group1);
     g.group_node(n1, group1);
     g.group_node(n2, group1);
 
     // Expecting 0 adjacent edges since no edge has been added
-    const auto& g1AdjacentEdgeSet = group1->get_adjacent_edges();
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 0 );
+    auto g1AdjacentEdgeSet = group1->collectAdjacentEdges();
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 0);
 
     // Adding an edge at graph level, expecting edge to be inserted in group adjacent edge set
-    g.create_edge( n1, n2 );
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 1 );
+    g.insert_edge(n1, n2);
+    g1AdjacentEdgeSet = group1->collectAdjacentEdges();
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 1);
 
     // Removing the edge at graph level, expecting group empty adjacent edge
-    g.remove_edge( n1, n2 );
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 0 );
+    g.remove_edge(n1, n2);
+    g1AdjacentEdgeSet = group1->collectAdjacentEdges();
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 0);
 
-    auto e2 = g.create_edge( n1, n2 );
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 1 );
+    g.insert_edge(n1, n2);
+    g1AdjacentEdgeSet = group1->collectAdjacentEdges();
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 1);
 
     // Removing node 1 from group, edge is still adjacent
     g.ungroup_node(n1, group1);
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 1 );
+    g1AdjacentEdgeSet = group1->collectAdjacentEdges();
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 1);
 
     // Removing node 2 from group, edge is no longer adjacent to group
     g.ungroup_node(n2, group1);
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 0 );
+    g1AdjacentEdgeSet = group1->collectAdjacentEdges();
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 0);
 }
 
-TEST(gtpo_group, adjacentEdges)
+TEST(gtpo_group, adjacent_edges_nodes)
 {
     qan::Graph g;
     auto n1 = g.create_node();
+    g.insert_node(n1);
     auto n2 = g.create_node();
+    g.insert_node(n2);
     auto n3 = g.create_node();
+    g.insert_node(n3);
     auto n4 = g.create_node();
+    g.insert_node(n4);
     auto n5 = g.create_node();
+    g.insert_node(n5);
 
-    auto e1 = g.create_edge( n1, n2 );
-    auto e2 = g.create_edge( n1, n3 );
-    auto e3 = g.create_edge( n2, n4 );
-    auto e4 = g.create_edge( n3, n5 );
-    auto e5 = g.create_edge( n4, n3 );
+    auto e1 = g.insert_edge(n1, n2);
+    auto e2 = g.insert_edge(n1, n3);
+    auto e3 = g.insert_edge(n2, n4);
+    auto e4 = g.insert_edge(n3, n5);
+    auto e5 = g.insert_edge(n4, n3);
 
-    auto g1 = g.create_group().lock();
+    auto g1 = new qan::Group{};
+    g.insert_group(g1);
     g.group_node(n3, g1);
     g.group_node(n4, g1);
     g.group_node(n5, g1);
 
     // See manual topology section, it is the same topology
     // Check that group 1 adjacent edge set is {e2, e3, e4, e5}
-    auto& g1AdjacentEdgeSet = g1->get_adjacent_edges();
-    EXPECT_TRUE( gtpo::find_weak_ptr( g1AdjacentEdgeSet, e2 ) );
-    EXPECT_TRUE( gtpo::find_weak_ptr( g1AdjacentEdgeSet, e3 ) );
-    EXPECT_TRUE( gtpo::find_weak_ptr( g1AdjacentEdgeSet, e4 ) );
-    EXPECT_TRUE( gtpo::find_weak_ptr( g1AdjacentEdgeSet, e5 ) );
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 4 );
+    auto g1AdjacentEdgeSet = g1->collectAdjacentEdges();
+    EXPECT_TRUE(g1AdjacentEdgeSet.find(e2) != g1AdjacentEdgeSet.end());
+    EXPECT_TRUE(g1AdjacentEdgeSet.find(e3) != g1AdjacentEdgeSet.end());
+    EXPECT_TRUE(g1AdjacentEdgeSet.find(e4) != g1AdjacentEdgeSet.end());
+    EXPECT_TRUE(g1AdjacentEdgeSet.find(e5) != g1AdjacentEdgeSet.end());
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 4);
 
     // Removing a node from the group should remove its in/out edges from group adjacent edge set
     // Here, removing n4 should remove e3 and e5
     g.ungroup_node(n4, g1);
-    EXPECT_EQ( g1AdjacentEdgeSet.size(), 3 );
+    g1AdjacentEdgeSet = g1->collectAdjacentEdges();
+    EXPECT_EQ(g1AdjacentEdgeSet.size(), 3);
 
     g.clear();
 }
-*/
