a4e96efe56875615cb97778f825dda1148f66fa8
Shantanu Tushar <shantanu.tushar.ext@siemens-energy.com>
2025-05-16 11:57:58 +0200

    Make sure to update EdgeItem based on the initial style
    Without this change, the edges will not use the style unless the style
    changes at least once.

--- src/qanEdgeItem.cpp
+++ src/qanEdgeItem.cpp
@@ -1,5 +1,6 @@
 /*
  Copyright (c) 2008-2024, Benoit AUTHEMAN All rights reserved.
+ Copyright (c) 2025, Siemens Energy Global GmbH & Co. KG
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
@@ -1225,6 +1226,7 @@ void    EdgeItem::setStyle(EdgeStyle* style) noexcept
                     this,      &EdgeItem::styleModified);
         }
         emit styleChanged();
+        styleModified();
         updateItem();   // Force initial style settings
     }
 }
